4f2321cae217dda61c62a8bbad24c42e138f86ca
Niels Hüsken <monarch@hotmail.de>
2018-05-27 22:46:49 +0200

    bootstrap und pdfviewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public pageNumber: number;
+  public title = 'app';
+  constructor() {
+    this.pageNumber = 1;
+  }
+  Increment() {
+    this.pageNumber++;
+  }
+
+  Decrement() {
+    this.pageNumber--;
+  }
+
+  ngOnInit(): void {
+  }
+
 }
